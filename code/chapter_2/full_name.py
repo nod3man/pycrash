8caa0b2c9e9bf3deac75f730184f55e0f4e780de
Alex Lisowski <nod3man@gmail.com>
2020-04-14 16:29:12 +0300

    Added code for "strings" from chapter 2

---
+++ code/chapter_2/full_name.py
@@ -0,0 +1,7 @@
+first_name = "alex"
+last_name = "lisowski"
+full_name=f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+
+print(message)
+# Prints: Hello, Alex Lisowski!
---
+++ code/chapter_2/name.py
@@ -0,0 +1,5 @@
+name = "alex lisowski"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
