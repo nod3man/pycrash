ba20ba9649b8cf93f6be784b1e45140c58f79156
Alex Lisowski <nod3man@gmail.com>
2020-04-14 19:22:43 +0300

    Added exercise solutions 2.8 & 2.9 (numbers)

---
+++ exercises/chapter_2/2.8_number_eight.py
@@ -0,0 +1,14 @@
+"""
+Write addition, subtraction, multiplication, and division
+operations that each result in the number 8. Be sure to enclose your operations
+in print() calls to see the results. You should create four lines that look like this:
+--------
+print(5+3)
+--------
+Your output should simply be four lines with the number 8 appearing once
+on each line.
+"""
+print(4+4)
+print(16-8)
+print(4*2)
+print(16/2)
---
+++ exercises/chapter_2/2.9_favorite_number.py
@@ -0,0 +1,8 @@
+"""
+Use a variable to represent your favorite number. Then,
+using that variable, create a message that reveals your favorite number. Print
+that message.
+"""
+number = 9
+message = f"My favorite number is {number}"
+print(message)
