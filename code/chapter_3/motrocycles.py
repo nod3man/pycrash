77ab7e3674d6bb23d8d9dcdc782c185cd3b1c85c
Alex Lisowski <nod3man@gmail.com>
2020-04-16 17:18:11 +0300

    Added remove method to motorcycles.py

--- code/chapter_3/motrocycles.py
+++ code/chapter_3/motrocycles.py
@@ -25,3 +25,6 @@ print(popped_motorcycle)
 popped_motorcycle = motorcycles.pop(0)
 print(motorcycles)
 print(popped_motorcycle)
+
+# Remove any item from the list using remove method
+motorcycles.remove("suzuki")
