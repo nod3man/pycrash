edd13d11d4f5eb4a7bcf6dbed3aa983b04cf6723
Alex Lisowski <nod3man@gmail.com>
2020-04-23 21:02:46 +0300

    Added code from Ch4

--- code/chapter_4/dimensions.py
+++ code/chapter_4/dimensions.py
@@ -2,3 +2,27 @@
 dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
+print()
+
+""" 
+It is possible to define tuples without parentheses
+dimensions = 300, 100
+print(dimensions[0])
+print(dimensions[1])
+"""
+
+# It is possible to loop through tuples
+for dimension in dimensions:
+    print(dimension)
+
+# It is possible to create new tuple with the same name
+print()
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+print()
+
+dimensions = (300, 100)
+print("Modified dimensions:")
+for dimension in dimensions:
+    print(dimension)
